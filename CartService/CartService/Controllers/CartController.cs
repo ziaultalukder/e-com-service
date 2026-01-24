e11cc856e7d9a1351425db99c9f6bbe41bd6dbc1
ziaul2023 <ziaul@primotechltd.com>
2026-01-24 16:12:45 +0600

    er

--- CartService/CartService/Controllers/CartController.cs
+++ CartService/CartService/Controllers/CartController.cs
@@ -21,6 +21,13 @@ namespace CartService.Controllers
         {
             return "value";
         }
+        
+        // GET api/<CartController>/5
+        [HttpGet("{id}")]
+        public string Get2(int id)
+        {
+            return "value";
+        }
 
         
     }
